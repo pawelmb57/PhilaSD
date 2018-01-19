6f2c51fe864ec6684430c6980d5fc5de7bb09f36
Pawel Bogdanowicz <p.m.bogdanowicz@gmail.com>
2018-01-19 14:06:46 -0500

    Add files via upload

---
+++ phillyDPO/R/testrounding.R
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+
+
+
+install.packages("devtools")
+library("devtools")
+
+install.packages("roxygen2")
+libary(roxygen2)
+
+library(stringr)
+
+
+setwd("D:/Git PhilaSD")
+
+
+
+create("phillyDPO")
+
+
+################################################################################
+#
+# Testing DF
+#
+################################################################################
+
+# df <- data.frame(
+#     col1 = seq(1:8)
+#     , col2 = c(runif(2), runif(2)*10000, rep("asdf",2), rep("12/12/2012",2))
+# )
+# df <- as.data.frame(lapply(df, as.character), stringsAsFactors = FALSE)
+# df
+
+
+
+
+
+################################################################################
+#
+# Rounding Function
+#
+################################################################################
+
+#' Round a number
+#'
+#' @param x A number or value that needs rounding
+#' @param n the number of digits the number should be rounded to
+#' @return A rounded number
+#' @examples
+#' round_dpo("123.123", 2)
+#' round_dpo(df$col, 2)
+
+
+round_dpo <- function(x, n){
+
+    x2 <- as.numeric(x)
+
+    z = sprintf("%.7f" , x2)
+    last4 <- as.numeric(str_sub(z , -4 , -1))
+    z = as.numeric(as.character(ifelse(last4 >= 9900 , as.numeric(z) + (10000-last4)/10^7 , as.numeric(z) )))
+    posneg = sign(x2)
+    z = as.numeric(paste(abs(z)*10^n + 0.5))
+    z = trunc(z)
+    z = z/10^n
+    x2 <- z*posneg
+
+    x2 <- ifelse(is.na(x2), x, x2)
+    return(x2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
