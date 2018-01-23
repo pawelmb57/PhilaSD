ccc5735c931708bf1a466b38ecc6f444a7117a98
Pawel Bogdanowicz <p.m.bogdanowicz@gmail.com>
2018-01-23 15:16:08 -0500

    Add files via upload

---
+++ phillyDPO/R/cleanCSV.R
@@ -0,0 +1,26 @@
+
+
+library(plyr)
+library(dplyr)
+
+setwd("D:/Git PhilaSD/phillyDPO/R")
+
+
+
+
+
+
+
+
+
+
+
+cleanCSV_dpo <- function(x){
+    x$key_for_cleanCSV_dpo <- seq(1:nrow(x))
+    x <- gather(x, columns_for_gather, values_for_gather, -key_for_cleanCSV_dpo)
+    x$values_for_gather <- ifelse(is.na(as.numeric(x$values_for_gather)), x$values_for_gather, as.numeric(x$values_for_gather))
+    x <- spread(x, columns_for_gather, values_for_gather)
+    x$key_for_cleanCSV_dpo <- NULL
+    x
+}
+
---
+++ phillyDPO/R/rounding_function.R
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+library(stringr)
+
+
+setwd("D:/Git PhilaSD")
+
+
+
+################################################################################
+#
+# Testing DF
+#
+################################################################################
+
+# df <- data.frame(
+#     col1 = seq(1:8)
+#     , col2 = c(runif(2), runif(2)*10000, rep("asdf",2), rep("12/12/2012",2))
+# )
+# df <- as.data.frame(lapply(df, as.character), stringsAsFactors = FALSE)
+# df
+
+
+
+
+
+################################################################################
+#
+# Rounding Function
+#
+################################################################################
+
+
+
+
+round_dpo <- function(x, n){
+
+    x2 <- as.numeric(x)
+
+    z = sprintf("%.7f" , x2)
+    last4 <- as.numeric(str_sub(z , -4 , -1))
+    z = as.numeric(as.character(ifelse(last4 >= 9900 , as.numeric(z) + (10000-last4)/10^7 , as.numeric(z) )))
+    posneg = sign(x2)
+    z = as.numeric(paste(abs(z)*10^n + 0.5))
+    z = trunc(z)
+    z = z/10^n
+    x2 <- z*posneg
+
+    x2 <- ifelse(is.na(x2), x, x2)
+    return(x2)
+}
+
+
+
+
+
+
+
+
+
+
+
