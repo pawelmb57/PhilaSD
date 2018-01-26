abb8bbd1a48ddf512e89ff102d6cb9fca952aa97
Pawel Bogdanowicz <p.m.bogdanowicz@gmail.com>
2018-01-26 12:01:58 -0500

    Add files via upload

--- phillyDPO/R/cleanCSV.R
+++ phillyDPO/R/cleanCSV.R
@@ -11,8 +11,16 @@ setwd("D:/Git PhilaSD/phillyDPO/R")
 
 
 
-
-
+# load("datapackets_combined.Rda")
+#
+#
+# # df <- df %>% select(SCN.School.ID, `School.Computer.Network.(SCN).ID.on.Record`)
+#
+#
+# df <- data.frame(
+#     col1 = c("1","2","asdf")
+#     , col2 = c(NA, "asdf","123.123")
+# )
 
 
 cleanCSV_dpo <- function(x){
@@ -23,4 +31,5 @@ cleanCSV_dpo <- function(x){
     x$key_for_cleanCSV_dpo <- NULL
     x
 }
+
 
--- phillyDPO/R/qa_tool.R
+++ phillyDPO/R/qa_tool.R
@@ -1,102 +1,118 @@
-#
-#
-#
-# library(haven)
-# library(tidyr)
-# library(dplyr)
-#
-# setwd("D:/Git PhilaSD/phillyDPO/R")
-#
-#
-#
-# #
-# #
-# # lw <- data.frame(lapply(lw, as.character), stringsAsFactors = FALSE)
-# # lw <- gather(lw, column_name, lw_val, -school_student)
-# #
-# # lw[lw == ""] <- NA
-# # lw$lw_col <- lw$column_name
-# #
-# #
-# #
-# #
-# # pb = data.frame(lapply(pb, as.character), stringsAsFactors = FALSE)
-# # pb = gather(pb, column_name, pb_val, -school_student)
-# #
-# # pb[pb == ""] <- NA
-# # pb$pb_col <- pb$column_name
-# #
-# #
-# #
-# # df <- merge(lw, pb, by=c("school_student","column_name"), all=T)
-# # df[is.na(df)] <- 999999
-# # df[df == ""]  <- 999999
-# #
-# #
-# #
-# # df$check <- ifelse(df$lw_val == df$pb_val, 0, 1)
-# #
-# #
-# # df <- df[!(df$column_name %in% c("literacy_assessment","numeracy_assessment")), ]
-# #
-# # # write.csv(unique(df[,c("lw_col","pb_col")]), "colCompare.csv", row.names = FALSE, na = "")
-# # write.csv(df, "compare.csv", row.names = FALSE, na = "")
-# #
-#
-#
-#
-#
-# rm(list=ls())
-#
-#
-#
-# load("lw.Rda")
-# load("pb.Rda")
-#
-#
-#
-# x <- lw
-# y <- pb
-# k <- "school_student"
-#
-# qa_dpo <- function(x,y,k){
-#
-#     # Check that x and y are dataframes
-#
-#
-#
-#
-#     x <- data.frame(lapply(x, as.character), stringsAsFactors = FALSE)
-#     x <- gather(x, column_name, x_val, -k)
-#     x[x == ""] <- NA
-#     x$x_col <- x$column_name
-#
-#
-#
-#
-#     y <- data.frame(lapply(y, as.character), stringsAsFactors = FALSE)
-#     y = gather(y, column_name, y_val, -k)
-#     y[y == ""] <- NA
-#     y$y_col <- y$column_name
-#
-#
-#
-#     df <- full_join(x, y, by=c(k, "column_name"))
-#
-#     df[is.na(df)] <- 99999
-#     df[df == ""] <- 99999
-#
-#     df$check <- ifelse(df$x_val == df$y_val, 0, 1)
-#
-#     qa_dpo_df <<- df
-# }
-# qa_dpo(lw, pb, "school_student")
-# qa_dpo_df[qa_dpo_df$school_student == "869 | 7014949" & qa_dpo_df$column_name == "initial_num_score", ]
-#
-#
-#
-#
-#
-#
-#
-#
+
+
+
+library(haven)
+library(tidyr)
+library(dplyr)
+
+setwd("D:/Git PhilaSD/phillyDPO/R")
+
+
+
+
+
+
+qa_dpo <- function(x,y,k){
+
+    # Matching on Columns
+    #
+    x_columns_df <- data.frame(x_columns = colnames(x), key = colnames(x), stringsAsFactors = FALSE)
+    y_columns_df <- data.frame(y_columns = colnames(y), key = colnames(y), stringsAsFactors = FALSE)
+    qa_dpo_columns <- full_join(x_columns_df, y_columns_df, by = "key")
+    qa_dpo_columns$column_check <- ifelse(qa_dpo_columns$x_columns == qa_dpo_columns$y_columns, 0,
+                                          ifelse(is.na(qa_dpo_columns$x_columns) | is.na(qa_dpo_columns$y_columns), 1, "ERROR"))
+
+
+    qa_dpo_columns$column_check <- ifelse(is.na(qa_dpo_columns$y_columns) | is.na(qa_dpo_columns$x_columns) , 1 ,
+                                          ifelse(qa_dpo_columns$x_columns == qa_dpo_columns$y_columns, 0, "Not Okay"))
+
+
+
+
+    # Cleaning Up
+    #
+    x <- data.frame(lapply(x, as.character), stringsAsFactors = FALSE)
+    x <- gather(x, column_name, x_val, -k)
+    x[x == ""] <- NA
+    x$x_col <- x$column_name
+
+    y <- data.frame(lapply(y, as.character), stringsAsFactors = FALSE)
+    y = gather(y, column_name, y_val, -k)
+    y[y == ""] <- NA
+    y$y_col <- y$column_name
+
+
+
+
+
+
+    df <- full_join(x, y, by=c(k, "column_name"))
+
+    df[is.na(df)] <- 99999
+    df[df == ""] <- 99999
+
+    df$key_for_cleanCSV_dpo <- seq(1:nrow(df))
+    df <- gather(df, columns_for_gather, values_for_gather, -key_for_cleanCSV_dpo)
+    df$values_for_gather <- ifelse(is.na(as.numeric(df$values_for_gather)), df$values_for_gather, as.numeric(df$values_for_gather))
+    df <- spread(df, columns_for_gather, values_for_gather)
+    df$key_for_cleanCSV_dpo <- NULL
+
+
+    df$check <- ifelse(df$x_val == df$y_val, 0, 1)
+
+
+
+
+
+
+    # cat(  "###############################################################################", "\n"
+    #     , "###############################################################################", "\n\n\n"
+    #     , "COLUMNS", "\n"
+    #     , "(One file has a column that the other does not)","\n"
+    #     , "###############################################################################", "\n\n"
+    #     ,  sum(qa_dpo_columns$column_check), "COLUMN MISMATCHES:", "There were", sum(qa_dpo_columns$column_check), "mismatches in columns", "\n\n"
+    #     # , "The columns are:", "\n", unique(qa_dpo_columns$key[qa_dpo_columns$column_check == 1]), "\n\n"
+    #     , "To view these columns, find qa_dpo_columns", "\n\n"
+    #
+    #
+    #     , "ROWS", "\n"
+    #     , "(After matching on the key, the files differ", "\n"
+    #     , "###############################################################################", "\n\n"
+    #     , sum(df$check), "VALUE MISMATCHES: There are a total of", sum(df$check), "mismatching data elemetns between the two files"
+    #     # , head(unique(df$column_name))
+    #     )
+
+
+
+
+    # if(sum(qa_dpo_columns$column_check > 0)){
+    #     qa_dpo_columns[qa_dpo_columns$column_check == 1, ]
+    # }
+
+
+
+
+
+    # qa_dpo_result <<- qa_dpo_list
+    qa_dpo_columns <<- qa_dpo_columns
+    qa_dpo_all <<- df
+    qa_dpo_mismatches <<- df[df$check == 1, ]
+
+
+    df %>%
+        filter(check == 1) %>%
+        group_by(column_name) %>%
+        dplyr::summarise(Number_of_Mismatches = n())
+
+
+
+}
+
+
+
+
+
+
+
+
+
